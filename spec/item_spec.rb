b331c221623a8d461e824f2671ac089d9e9ea676
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-17 09:15:30 -0600

    add item class and spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,21 @@
 require 'rspec'
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@item1).to be_a(Item)
+    end
+
+    it 'has attributes' do
+      expect(@item2.name).to eq('Tomato')
+      expect(@item2.price).to eq(0.5)
+    end
+  end
+end
